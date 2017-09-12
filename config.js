eeae705bd46e6ad86cbe8b7ff5b1c323f56c72ae
Caio Chaves <caaiochavez@gmail.com>
2017-09-12 10:52:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = '';
 global.EMAIL_TMPL = 'Iae, <strong>{0}</strong>, voçê está cadastrado no meu sistema!!!';
 
 module.exports = {
 	connectionString : 'mongodb://localhost/pessoas',
-	sendgridKey: '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',
+	sendgridKey: '',
 	containerConnectionString: 'TBD'
 }
